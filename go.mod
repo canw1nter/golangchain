a1a22b028afdae108a2a23b8b8274b0354c239e0
yanghuidong <yanghuidong@doodod.com>
2023-07-10 09:48:25 +0800

    feat(OpenAIModel) 构造函数添加

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module golangchain
 
 go 1.20
 
-require github.com/sashabaranov/go-openai v1.13.0 // indirect
+require github.com/sashabaranov/go-openai v1.13.0
--- models/openai.go
+++ models/openai.go
@@ -7,11 +7,17 @@ import (
 )
 
 type OpenAIModel struct {
+	APIKey string
+	*OpenAIModelOption
+}
+
+type OpenAIModelOption struct {
+	MaxToken    int
 	ModelName   string
 	Temperature float32
-	APIKey      string
-	MaxToken    int
 }
+
+type OptionSet func(option *OpenAIModelOption)
 
 func (opm *OpenAIModel) Generate() {
 	client := openai.NewClient(opm.APIKey)
@@ -36,4 +42,23 @@ func (opm *OpenAIModel) Generate() {
 	}
 
 	fmt.Println(resp.Choices[0].Message.Content)
+}
+
+func NewOpenAIModel(apiKey string, optionSets ...OptionSet) *OpenAIModel {
+	option := &OpenAIModelOption{
+		ModelName:   "gpt-3.5-turbo-0613",
+		MaxToken:    400,
+		Temperature: 0.0,
+	}
+
+	for _, optionSet := range optionSets {
+		optionSet(option)
+	}
+
+	model := &OpenAIModel{
+		APIKey: apiKey,
+	}
+	model.OpenAIModelOption = option
+
+	return model
 }
